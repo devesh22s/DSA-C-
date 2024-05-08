a0dd47153fb3de7397469ad2332b3ee0a4fb9d4b
devesh22s <139629713+devesh22s@users.noreply.github.com>
2024-05-08 12:26:56 +0530

    finding the value more appears more than one time

---
+++ vectors/more_then_one_value.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+
+
+int search(vector<int>& arr, int target){
+    int count =0;
+    for(int i =0; i <arr.size(); i++){
+        if(arr[i] == target){
+            count++;
+        }
+    }
+
+    cout <<"The Target value "<<target<<" appears "<<count<<" times "<<endl;
+
+}
+
+int main(){
+    vector<int> arr = {4,14,2,55,7,2,6,9,4,4};
+    int target = 4;
+
+    int max = arr[0];
+    for(int i =0; i <arr.size(); i++){
+        if(arr[i] > max){
+            max = arr[i];
+            
+        }
+    }
+    
+
+   search(arr, target);
+
+   cout<<"the maximum value in array is :"<<max;
+    
+    
+}
